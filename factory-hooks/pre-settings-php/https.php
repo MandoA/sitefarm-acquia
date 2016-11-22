3eefb0e32d39eee10209d55390f51df1926901ab
Shawn DeArmond <sgdearmond@ucdavis.edu>
2016-11-22 13:40:41 -0800

    SITEFARM-928 Testing on Acquia servers.

--- factory-hooks/pre-settings-php/https.php
+++ factory-hooks/pre-settings-php/https.php
@@ -2,6 +2,14 @@
 /**
  * Redirects *.ucdavis.edu traffic to https://
  */
+error_log("AH_SITE_ENVIRONMENT: ". $_SERVER['AH_SITE_ENVIRONMENT']);
+error_log("HTTPS: ". $_SERVER['HTTPS']);
+error_log("HTTP_X_SSL: ". $_SERVER['HTTP_X_SSL']);
+error_log("HTTP_HOST: ". $_SERVER['HTTP_HOST']);
+error_log("REQUEST_URI: ". $_SERVER['REQUEST_URI']);
+error_log("php_sapi_name: ". php_sapi_name());
+error_log("preg_match: ". preg_match("/ucdavis\.edu$/", $_SERVER['HTTP_HOST']));
+
 if (isset($_SERVER['AH_SITE_ENVIRONMENT']) &&
   ($_SERVER['HTTPS'] === 'OFF') &&
   preg_match("/ucdavis\.edu$/", $_SERVER['HTTP_HOST']) &&
