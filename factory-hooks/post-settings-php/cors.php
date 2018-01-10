6c3c5aaa3677ff824108757a6ca68337593f78a1
Shawn DeArmond <sgdearmond@ucdavis.edu>
2018-01-10 10:48:53 -0800

    SITEFARM-1387 container_yamls looking to site folder now.

--- factory-hooks/post-settings-php/cors.php
+++ factory-hooks/post-settings-php/cors.php
@@ -5,4 +5,4 @@
  *
  * This file will be included into settings.php for all sites on Site Factory.
  */
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/themes/site/cors.services.yml';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/themes/site/cors.services.yml';
