dc5aaa7eee214d8980ee1490050a1720f7649898
Shawn DeArmond <sgdearmond@ucdavis.edu>
2018-07-16 09:43:42 -0700

    SITEFARM-1654 Added logic to factory hooks to redirect ucdsitefarm.acsitefactory.com to sf.ucdavis.edu

--- factory-hooks/post-settings-php/https.php
+++ factory-hooks/post-settings-php/https.php
@@ -1,4 +1,28 @@
 <?php
+
+/**
+ * Redirects ucdsitefarm.acsitefactory.com traffic to sf.ucdavis.edu
+ */
+if (isset($_SERVER['AH_SITE_ENVIRONMENT']) &&
+    $match = preg_match("/(.*)\.(dev|test|)-?ucdsitefarm\.acsitefactory\.com$/", $_SERVER['HTTP_HOST']) &&
+    php_sapi_name() != "cli") {
+  $site = strip_tags($match[1]);
+  $env = strip_tags($match[2]);
+  switch ($env) {
+    case '':
+      $domain = $site . '.sf.ucdavis.edu';
+      break;
+    case 'test':
+      $domain = $site . '.test.sf.ucdavis.edu';
+      break;
+    case 'dev':
+      $domain = $site . '.dev.sf.ucdavis.edu';
+      break;
+  }
+  header('HTTP/1.0 301 Moved Permanently');
+  header('Location: https://' . $domain . $_SERVER['REQUEST_URI']);
+}
+
 /**
  * Redirects *.ucdavis.edu traffic to https://
  */
@@ -7,6 +31,6 @@ if (isset($_SERVER['AH_SITE_ENVIRONMENT']) &&
   preg_match("/ucdavis\.edu$/", $_SERVER['HTTP_HOST']) &&
   (php_sapi_name() != "cli")) {
     header('HTTP/1.0 301 Moved Permanently');
-    header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     exit();
 }
