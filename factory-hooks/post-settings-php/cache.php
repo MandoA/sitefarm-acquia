a6a2ad6e5701126eb12b1f14e00a493521b79dee
Shawn DeArmond <sgdearmond@ucdavis.edu>
2018-10-02 13:35:12 -0700

    SITEFARM-687 forgot the semicolon

--- factory-hooks/post-settings-php/cache.php
+++ factory-hooks/post-settings-php/cache.php
@@ -6,4 +6,4 @@
  * This file will be included into settings.php for all sites on Site Factory.
  */
 // Set max age for Varnish cache as defined in https://docs.acquia.com/site-factory/manage/website/cache/modify/
-$config['page_cache_maximum_age'] = 21600
+$config['page_cache_maximum_age'] = 21600;
