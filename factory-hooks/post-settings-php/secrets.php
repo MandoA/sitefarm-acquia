95b14d07966e1d8071970967581ae23d2111a536
Shawn DeArmond <sgdearmond@ucdavis.edu>
2020-12-07 17:58:25 -0800

    release-8.3.0 using env ah_site_group

--- factory-hooks/post-settings-php/secrets.php
+++ factory-hooks/post-settings-php/secrets.php
@@ -5,7 +5,7 @@
  *
  * This file will be included into settings.php for all sites on Site Factory.
  */
-$secrets_file = '/home/ucdsitefarm/secrets/secrets.settings.php';
+$secrets_file = sprintf('/home/%s/secrets/secrets.settings.php', $_ENV['AH_SITE_GROUP']);
 if (file_exists($secrets_file)) {
   require $secrets_file;
 }
