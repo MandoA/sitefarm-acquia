215aea199852a75cb535076ef81c44d759fb8ba2
Shawn DeArmond <sgdearmond@ucdavis.edu>
2022-11-22 13:42:41 -0800

    SITEFARM-2991 Added config override for samlauth. Renamed factory hook cas.php to auth.php

--- factory-hooks/post-settings-php/auth.php
+++ factory-hooks/post-settings-php/auth.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * @file
- * CAS module settings.
+ * Authentication settings.
  *
  * This file will be included into settings.php for all sites on Site Factory.
  */
+
+// CAS module settings.
 if (isset($_SERVER['AH_SITE_ENVIRONMENT']) &&
   in_array($_SERVER['AH_SITE_ENVIRONMENT'], ['01live', '01update'])
   ) {
@@ -24,3 +26,18 @@ $config['cas.settings']['user_accounts']['restrict_password_management'] = TRUE;
 $config['cas.settings']['email_hostname'] = 'ucdavis.edu';
 $config['cas.settings']['login_link_enabled'] = TRUE;
 $config['cas.settings']['login_link_label'] = 'UC Davis Login';
+
+// samlauth module settings.
+$config['samlauth.authentication']['map_users'] = TRUE;
+$config['samlauth.authentication']['map_users_name'] = FALSE;
+$config['samlauth.authentication']['map_users_mail'] = TRUE;
+$config['samlauth.authentication']['map_users_roles'] = [
+  'site_manager' => 'site_manager',
+  'administrator' => 'administrator',
+  'contributor' => '0',
+  'editor' => '0',
+  'site_builder' => '0'
+  ];
+$config['samlauth.authentication']['create_users'] = TRUE;
+$config['samlauth.authentication']['sync_name'] = TRUE;
+$config['samlauth.authentication']['sync_mail'] = TRUE;
