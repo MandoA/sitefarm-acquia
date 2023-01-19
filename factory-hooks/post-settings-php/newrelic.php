46b8dda1ced88fc0ca455fd499c7791524800189
Shawn DeArmond <sgdearmond@ucdavis.edu>
2023-01-19 13:27:04 -0800

    SITEFARM-3026 Added new code from Acquia Support ticket #00914985

--- factory-hooks/post-settings-php/newrelic.php
+++ factory-hooks/post-settings-php/newrelic.php
@@ -8,8 +8,10 @@
  */
 
 if (extension_loaded('newrelic')) {
-  $env = $_ENV['AH_SITE_ENVIRONMENT'] ?? 'local';
-
+// Get all the domains that are defined for the current site.
+  $domains = gardens_data_get_sites_from_file($GLOBALS['gardens_site_settings']['conf']['acsf_db_name']);
+// Get the site's name from the first domain.
   global $_acsf_site_name;
+  $_acsf_site_name = explode('.', array_keys($domains)[0])[0];
   newrelic_set_appname("ucdsitefarm.$env.$_acsf_site_name; ucdsitefarm.$env", '', 'true');
 }
